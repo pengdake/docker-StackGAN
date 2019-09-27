59e316e9955993cacd29721a4ef53f6b2e42039a
pengdake <19921207pq@gmail.com>
2019-09-28 01:52:09 +0800

    remove dataset about birds

--- download_data.sh
+++ download_data.sh
@@ -12,22 +12,14 @@ function download() {
 }
 
 cd StackGAN/Data
-echo "Downloading bird embeddings..."
-download "https://github.com/brangerbriz/docker-StackGAN/releases/download/datasets/birds.zip"
 echo "Downloading flower embeddings..."
 download "https://github.com/brangerbriz/docker-StackGAN/releases/download/datasets/flowers.zip"
 
-cd birds
-echo "Downloading bird images..."
-download "https://github.com/brangerbriz/docker-StackGAN/releases/download/datasets/CUB_200_2011.zip"
-
-cd ../flowers
+cd flowers
 echo "Downloading flower images..."
 download "https://github.com/brangerbriz/docker-StackGAN/releases/download/datasets/102flowers.zip"
 
 cd ../..
-echo "Preprocessing bird images..."
-python "misc/preprocess_birds.py"
 echo "Preprocessing flower images..."
 python "misc/preprocess_flowers.py"
 
--- download_models.sh
+++ download_models.sh
@@ -6,16 +6,8 @@ function download() {
 }
 
 cd StackGAN/models
-echo "Downloading pretrained birds model..."
-download "https://github.com/brangerbriz/docker-StackGAN/releases/download/datasets/birds_model_164000.ckpt"
-echo "Downloading pretrained flowers model..."
-download "https://github.com/brangerbriz/docker-StackGAN/releases/download/datasets/flowers_model_130000.ckpt"
-echo "Downloading pretrained birds skip thought model..."
-download "https://github.com/brangerbriz/docker-StackGAN/releases/download/datasets/birds_skip_thought_model_164000.ckpt"
 
 mkdir -p text_encoder
 cd text_encoder
-echo "Downloading text encoder for birds model..."
-download "https://github.com/brangerbriz/docker-StackGAN/releases/download/datasets/lm_sje_nc4_cub_hybrid_gru18_a1_c512_0.00070_1_10_trainvalids.txt_iter30000.t7"
 echo "Downloading text encoder for flowers model..."
 download "https://github.com/brangerbriz/docker-StackGAN/releases/download/datasets/lm_sje_flowers_c10_hybrid_0.00070_1_10_trainvalids.txt_iter16400.t7"
